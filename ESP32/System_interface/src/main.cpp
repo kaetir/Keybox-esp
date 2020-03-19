9347f5d224c46a6dcc1c4eb98626454b20becc90
Antonoir <avalletonin@gmail.com>
2020-03-19 10:47:56 +0100

    Ajout du dossier de langue des menu

--- ESP32/System_interface/src/main.cpp
+++ ESP32/System_interface/src/main.cpp
@@ -1,10 +1,26 @@
 #include <Arduino.h>
 #include "Interface.h"
+#define TOUCH_1 27
+#define TOUCH_2 14
+#define TOUCH_3 12
+#define TOUCH_4 13
+#define TOUCH_OK 32
+#define TOUCH_BACK 33
 
+//DESCRIPTION: initialize the serial communications
+//PARAMS: none
+//RETURN VALUE: none
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
 }
 
+//DESCRIPTION: main loop to update the interface
+//PARAMS: none
+//RETURN VALUE: none
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  if(touchRead(TOUCH_1) < 50 || touchRead(TOUCH_2) < 50 || touchRead(TOUCH_3) < 50  || touchRead(TOUCH_4) < 50 || touchRead(TOUCH_OK) < 50 || touchRead(TOUCH_BACK) < 50 ){
+
+
+  }
 }
