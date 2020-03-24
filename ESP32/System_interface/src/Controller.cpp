c352d4152ee30ca61af3668121dcc0d38c6e9e26
Antonoir <avalletonin@gmail.com>
2020-03-24 10:09:43 +0100

    Adding has_input() in Controller

--- ESP32/System_interface/src/Controller.cpp
+++ ESP32/System_interface/src/Controller.cpp
@@ -63,3 +63,19 @@ int Controller::get_input(){
 
     }
 }
+
+bool Controller::has_input(){
+    int button_left_val, button_right_val, button_top_val, button_bottom_val, button_ok_val, button_back_val;
+    button_left_val = touchRead(BUTTON_LEFT);
+    button_right_val = touchRead(BUTTON_RIGHT);
+    button_top_val = touchRead(BUTTON_TOP);
+    button_bottom_val = touchRead(BUTTON_BOTTOM);
+    button_ok_val = touchRead(BUTTON_OK);
+    button_back_val = touchRead(BUTTON_BACK);
+
+    if(button_left_val < THRESHOLD_VAL || button_right_val < THRESHOLD_VAL || button_top_val < THRESHOLD_VAL || button_bottom_val < THRESHOLD_VAL || button_ok_val < THRESHOLD_VAL || button_back_val < THRESHOLD_VAL){
+        return true; //input detected
+    }else{
+        return false; //No input detected
+    }
+}
