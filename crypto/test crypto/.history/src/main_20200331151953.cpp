2deaa0bcea0dde27df27c64ee0de977ea5fce208
jean-loup <jeanloup.greff@gmail.com>
2020-03-31 15:22:21 +0200

    a little bit cleaner code

---
+++ crypto/test crypto/.history/src/main_20200331151953.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+#include "RandomPwdGenerator.h"
+#include "PwdCypher.h"
+#include <iostream>
+#include <string>
+using namespace std;
+int n = 0;
+char key[] = "concombre";
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("1");
+  setup_hash();
+  vTaskDelay(500);
+  Serial.print("2");
+  setup_AES();
+  vTaskDelay(500);
+}
