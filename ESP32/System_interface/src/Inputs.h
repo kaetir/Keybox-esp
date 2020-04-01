5c3f38a426c26e2abaf931c979ed26c5ed0921d6
Antonoir <avalletonin@gmail.com>
2020-04-01 11:25:50 +0200

    AJout commentaire dans Inputs

--- ESP32/System_interface/src/Inputs.h
+++ ESP32/System_interface/src/Inputs.h
@@ -7,8 +7,31 @@ class Inputs{
     private:
 
     public:
+    /** 
+     * @Desc:  Constructor
+     * @Params:  none
+     * @Returns:  none
+     */
     Inputs();
+
+    /** 
+     * @Desc:  Destructor
+     * @Params:  none
+     * @Returns:  none
+     */
     ~Inputs();
+
+    /** 
+     * @Desc:  Check if there is an input from the user
+     * @Params:  none
+     * @Returns:  Return True if there is an input and False if there is no input
+     */
     bool has_input();
+
+    /** 
+     * @Desc:  Get the input from the user
+     * @Params:  none
+     * @Returns:  none
+     */
     int get_input();
 };
