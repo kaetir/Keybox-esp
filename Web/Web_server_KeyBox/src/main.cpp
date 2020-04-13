c53d9d9a122baccb0a8070e318c7e128df4c79af
Desprez Thibault <thibault.desprez1@gmail.com>
2020-04-13 13:03:23 +0200

    web serveur HTTPS SPIFFS fonctionnel

--- Web/Web_server_KeyBox/src/main.cpp
+++ Web/Web_server_KeyBox/src/main.cpp
@@ -34,10 +34,26 @@
 #include <WiFi.h>
 
 // Includes for the server
-#include <HTTPSServer.hpp>
-#include <SSLCert.hpp>
 #include <HTTPRequest.hpp>
 #include <HTTPResponse.hpp>
+#include <HTTPSServer.hpp>
+#include <SSLCert.hpp>
+
+// We will use SPIFFS and FS
+#include <FS.h>
+#include <SPIFFS.h>
+// We need to specify some content-type mapping, so the resources get delivered with the
+// right content type and are displayed correctly in the browser
+char contentTypes[][2][32] = {
+    { ".htm", "text/html" },
+    { ".html", "text/html" },
+    { ".css", "text/css" },
+    { ".js", "application/javascript" },
+    { ".json", "application/json" },
+    { ".png", "image/png" },
+    { ".jpg", "image/jpg" },
+    { "", "" }
+};
 
 // The HTTPS Server comes in a separate namespace. For easier use, include it here.
 using namespace httpsserver;
@@ -51,122 +67,242 @@ SSLCert cert = SSLCert(
 HTTPSServer secureServer = HTTPSServer(&cert);
 
 // Declare some handler functions for the various URLs on the server
-void handleRoot(HTTPRequest *req, HTTPResponse *res);
-void handle404(HTTPRequest *req, HTTPResponse *res);
+void handleRoot(HTTPRequest* req, HTTPResponse* res);
+void handle404(HTTPRequest* req, HTTPResponse* res);
+void handleRessources(HTTPRequest* req, HTTPResponse* res);
+void handleSPIFFS(HTTPRequest* req, HTTPResponse* res);
 
 // We declare a function that will be the entry-point for the task that is going to be
 // created.
-void serverTask(void *params);
+void serverTask(void* params);
 
 void setup()
 {
-  // For logging
-  Serial.begin(115200);
+    // For logging
+    Serial.begin(115200);
 
-  // Connect to WiFi
-  Serial.println("Setting up WiFi");
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.print("Connected. IP=");
-  Serial.println(WiFi.localIP());
+    // Try to mount SPIFFS without formatting on failure
+    if (!SPIFFS.begin(false)) {
+        // If SPIFFS does not work, we wait for serial connection...
+        while (!Serial)
+            ;
+        delay(1000);
 
-  // Setup the server as a separate task.
-  Serial.println("Creating server task... ");
-  // We pass:
-  // serverTask - the function that should be run as separate task
-  // "https443" - a name for the task (mainly used for logging)
-  // 6144       - stack size in byte. If you want up to four clients, you should
-  //              not go below 6kB. If your stack is too small, you will encounter
-  //              Panic and stack canary exceptions, usually during the call to
-  //              SSL_accept.
-  xTaskCreatePinnedToCore(serverTask, "https443", 6144, NULL, 1, NULL, ARDUINO_RUNNING_CORE);
-}
+        // Ask to format SPIFFS using serial interface
+        Serial.print("Mounting SPIFFS failed. Try formatting? (y/n): ");
+        while (!Serial.available())
+            ;
+        Serial.println();
 
-void loop()
-{
-  Serial.println("loop()");
-  delay(5000);
-}
-
-void serverTask(void *params)
-{
-  // In the separate task we first do everything that we would have done in the
-  // setup() function, if we would run the server synchronously.
-
-  // Note: The second task has its own stack, so you need to think about where
-  // you create the server's resources and how to make sure that the server
-  // can access everything it needs to access. Also make sure that concurrent
-  // access is no problem in your sketch or implement countermeasures like locks
-  // or mutexes.
-
-  // Create nodes
-  ResourceNode *nodeRoot = new ResourceNode("/", "GET", &handleRoot);
-  ResourceNode *node404 = new ResourceNode("", "GET", &handle404);
-
-  // Add nodes to the server
-  secureServer.registerNode(nodeRoot);
-  secureServer.setDefaultNode(node404);
-
-  Serial.println("Starting server...");
-  secureServer.start();
-  if (secureServer.isRunning())
-  {
-    Serial.println("Server ready.");
-
-    // "loop()" function of the separate task
-    while (true)
-    {
-      // This call will let the server do its work
-      secureServer.loop();
-
-      // Other code would go here...
-      delay(1);
+        // If the user did not accept to try formatting SPIFFS or formatting failed:
+        if (Serial.read() != 'y' || !SPIFFS.begin(true)) {
+            Serial.println("SPIFFS not available. Stop.");
+            while (true)
+                ;
+        }
+        Serial.println("SPIFFS has been formated.");
     }
-  }
+    Serial.println("SPIFFS has been mounted.");
+    File root = SPIFFS.open("/");
+
+    File file = root.openNextFile();
+
+    while (file) {
+
+        Serial.print("FILE: ");
+        Serial.println(file.name());
+
+        file = root.openNextFile();
+    }
+    // Connect to WiFi
+    Serial.println("Setting up WiFi");
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.print("Connected. IP=");
+    Serial.println(WiFi.localIP());
+
+    // Setup the server as a separate task.
+    Serial.println("Creating server task... ");
+    // We pass:
+    // serverTask - the function that should be run as separate task
+    // "https443" - a name for the task (mainly used for logging)
+    // 6144       - stack size in byte. If you want up to four clients, you should
+    //              not go below 6kB. If your stack is too small, you will encounter
+    //              Panic and stack canary exceptions, usually during the call to
+    //              SSL_accept.
+    xTaskCreatePinnedToCore(serverTask, "https443", 6144, NULL, 1, NULL, ARDUINO_RUNNING_CORE);
 }
 
-void handleRoot(HTTPRequest *req, HTTPResponse *res)
-{
-  // Status code is 200 OK by default.
-  // We want to deliver a simple HTML page, so we send a corresponding content type:
-  res->setHeader("Content-Type", "text/html");
+void loop() {}
 
-  // The response implements the Print interface, so you can use it just like
-  // you would write to Serial etc.
-  res->println("<!DOCTYPE html>");
-  res->println("<html>");
-  res->println("<head><title>Hello World!</title></head>");
-  res->println("<body>");
-  res->println("<h1>Hello World!</h1>");
-  res->print("<p>Your server is running for ");
-  // A bit of dynamic data: Show the uptime
-  res->print((int)(millis() / 1000), DEC);
-  res->println(" seconds.</p>");
-  res->println("</body>");
-  res->println("</html>");
+void serverTask(void* params)
+{
+    // In the separate task we first do everything that we would have done in the
+    // setup() function, if we would run the server synchronously.
+
+    // Note: The second task has its own stack, so you need to think about where
+    // you create the server's resources and how to make sure that the server
+    // can access everything it needs to access. Also make sure that concurrent
+    // access is no problem in your sketch or implement countermeasures like locks
+    // or mutexes.
+
+    // Create nodes
+    ResourceNode* spiffsNode = new ResourceNode("", "", &handleSPIFFS);
+    ResourceNode* nodeRoot = new ResourceNode("/", "GET", &handleRoot);
+    ResourceNode* nodeQueryDemo = new ResourceNode("/index", "GET", &handleRessources);
+
+    // Add nodes to the server
+    // We register the SPIFFS handler as the default node, so every request that does
+    // not hit any other node will be redirected to the file system.
+    secureServer.setDefaultNode(spiffsNode);
+    secureServer.registerNode(nodeRoot);
+    secureServer.registerNode(nodeQueryDemo);
+
+    Serial.println("Starting server...");
+    secureServer.start();
+    if (secureServer.isRunning()) {
+        Serial.println("Server ready.");
+
+        // "loop()" function of the separate task
+        while (true) {
+            // This call will let the server do its work
+            secureServer.loop();
+
+            // Other code would go here...
+            delay(1);
+        }
+    }
 }
 
-void handle404(HTTPRequest *req, HTTPResponse *res)
+void handleRoot(HTTPRequest* req, HTTPResponse* res)
 {
-  // Discard request body, if we received any
-  // We do this, as this is the default node and may also server POST/PUT requests
-  req->discardRequestBody();
+    // Status code is 200 OK by default.
+    // We want to deliver a simple HTML page, so we send a corresponding content type:
+    res->setHeader("Content-Type", "text/html");
 
-  // Set the response status
-  res->setStatusCode(404);
-  res->setStatusText("Not Found");
+    // The response implements the Print interface, so you can use it just like
+    // you would write to Serial etc.
+    res->println("<!DOCTYPE html>");
+    res->println("<html>");
+    res->println("<head><title>UpTime</title></head>");
+    res->println("<body>");
+    res->print("<p>Your server is running for ");
+    // A bit of dynamic data: Show the uptime
+    res->print((int)(millis() / 1000), DEC);
+    res->println(" seconds.</p>");
+    res->println("</body>");
+    res->println("</html>");
+}
 
-  // Set content type of the response
-  res->setHeader("Content-Type", "text/html");
+// This is a more generic demo for the query parameters. It makes use of the iterator
+// interface to access them, which is useful if you do not know the paramter names in
+// adavance.
+void handleRessources(HTTPRequest* req, HTTPResponse* res)
+{
+    // A word of warning: In this example, we use the query parameters and directly print
+    // them into the HTML output. We do this to simplify the demo. NEVER do this in a
+    // real application, as it allows cross-site-scripting.
+    res->setHeader("Content-Type", "text/html");
 
-  // Write a tiny HTTP page
-  res->println("<!DOCTYPE html>");
-  res->println("<html>");
-  res->println("<head><title>Not Found</title></head>");
-  res->println("<body><h1>404 Not Found</h1><p>The requested resource was not found on this server.</p></body>");
-  res->println("</html>");
+    res->println("<!DOCTYPE html>");
+    res->println("<html>");
+    res->println("<head>");
+    res->println("<title>Query Parameter Demo</title>");
+    res->println("</head>");
+    res->println("<body>");
+    res->println("<p>The following query paramters have been set:</p>");
+
+    // Start a table to display the parameters
+    res->println("<table style=\"border:1px solid black collapse;\">");
+    res->println("<tr><th>Key</th><th>Value</th></tr>");
+    // Iterate over the parameters. For more information, read about the C++ standard template library,
+    // especially about vectors and iterators.
+    ResourceParameters* params = req->getParams();
+    for (auto it = params->beginQueryParameters(); it != params->endQueryParameters(); ++it) {
+        res->print("<tr><td>");
+
+        // The iterator yields std::pairs of std::strings. The first value contains the parameter key
+        res->printStd((*it).first);
+        res->print("</td><td>");
+
+        // and the second value contains the parameter value
+        res->printStd((*it).second);
+        res->println("</td></tr>");
+    }
+    res->println("</table>");
+
+    // You can retrieve the total parameter count from the parameters instance:
+    res->print("<p>There are a total of ");
+    res->print(params->getQueryParameterCount());
+    res->print(" parameters, with ");
+    res->print(params->getQueryParameterCount(true));
+    res->println(" unique keys.</p>");
+
+    res->println("<p>Go <a href=\"/\">back to main page</a>.</p>");
+    res->println("</body>");
+    res->println("</html>");
+}
+
+/**
+ * This handler function will try to load the requested resource from SPIFFS's /public folder.
+ * 
+ * If the method is not GET, it will throw 405, if the file is not found, it will throw 404.
+ */
+void handleSPIFFS(HTTPRequest* req, HTTPResponse* res)
+{
+
+    // We only handle GET here
+    if (req->getMethod() == "GET") {
+        // Redirect / to /index.html
+        std::string reqFile = req->getRequestString() == "/" ? "/index.htm" : req->getRequestString();
+
+        // Try to open the file
+        std::string filename = std::string("") + reqFile;
+
+        // Check if the file exists
+        if (!SPIFFS.exists(filename.c_str())) {
+            Serial.print("File not found :");
+            Serial.println(filename.c_str());
+            // Send "404 Not Found" as response, as the file doesn't seem to exist
+            res->setStatusCode(404);
+            res->setStatusText("Not found");
+            res->println("404 Not Found");
+            return;
+        }
+
+        File file = SPIFFS.open(filename.c_str());
+
+        // Set length
+        res->setHeader("Content-Length", httpsserver::intToString(file.size()));
+
+        // Content-Type is guessed using the definition of the contentTypes-table defined above
+        int cTypeIdx = 0;
+        do {
+            if (reqFile.rfind(contentTypes[cTypeIdx][0]) != std::string::npos) {
+                res->setHeader("Content-Type", contentTypes[cTypeIdx][1]);
+                break;
+            }
+            cTypeIdx += 1;
+        } while (strlen(contentTypes[cTypeIdx][0]) > 0);
+
+        // Read the file and write it to the response
+        uint8_t buffer[256];
+        size_t length = 0;
+        do {
+            length = file.read(buffer, 256);
+            res->write(buffer, length);
+        } while (length > 0);
+
+        file.close();
+    } else {
+        // If there's any body, discard it
+        req->discardRequestBody();
+        // Send "405 Method not allowed" as response
+        res->setStatusCode(405);
+        res->setStatusText("Method not allowed");
+        res->println("405 Method not allowed");
+    }
 }
